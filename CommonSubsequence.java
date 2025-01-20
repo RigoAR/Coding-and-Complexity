ba0ad2d1983f8466504686117ace6a086d42c31c
RigoAR <rigobertoalvaradorayo@gmail.com>
2025-01-20 17:26:29 -0600

    first problem

---
+++ CommonSubsequence.java
@@ -0,0 +1,30 @@
+
+public class CommonSubsequence
+{
+    public static void main(String[] args)
+    {
+        String text1 = "albatross";
+        String text2 = "almanacs";
+        CommonSubsequence start = new CommonSubsequence();
+        System.out.println("The number of common subsequence is " + start.find(text1, text2));
+    }
+
+    public static int find(String text1, String text2)
+    {
+        int num = 0;
+        for(int i = 0; i < text1.length(); i++)
+        {
+            for(int j = 0; j < text2.length(); j++)
+            {
+                if (text1.charAt(i) == text2.charAt(j))
+                {
+                    num++;
+                    break;
+                }
+            }
+        }
+
+            return num;
+
+    }
+}
