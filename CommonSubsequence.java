e11254925592b903d391b8c1b65d2b3396379608
RigoAR <rigobertoalvaradorayo@gmail.com>
2025-01-20 18:35:04 -0600

    first problem fixed

--- CommonSubsequence.java
+++ CommonSubsequence.java
@@ -12,13 +12,16 @@ public class CommonSubsequence
     public static int find(String text1, String text2)
     {
         int num = 0;
+        boolean[] used = new boolean[text2.length()];
+
         for(int i = 0; i < text1.length(); i++)
         {
             for(int j = 0; j < text2.length(); j++)
             {
-                if (text1.charAt(i) == text2.charAt(j))
+                if (text1.charAt(i) == text2.charAt(j) && !used[j])
                 {
                     num++;
+                    used[j] = true;
                     break;
                 }
             }
--- CommonSubstring.java
+++ CommonSubstring.java
@@ -1,41 +1,33 @@
+/*
 public class CommonSubstring {
+    public static void main(String[] args)
+    {
+        String text1 = "gears of war";
+        String text2 = "history of warriors";
+        CommonSubstring start = new CommonSubstring();
+        System.out.println("The number of common subsequence is " + start.find(text1, text2));
+    }
+
+    public static String[] find(String text1, String text2)
+    {
+        String[] num = new String[text1.length() + text2.length()];
 
 
-
-        public static void main(String[] args)
+        for(int i = 0; i < text1.length(); i++)
         {
-            String text1 = "gears of war";
-            String text2 = "history of warriors";
-            CommonSubstring start = new CommonSubstring();
-            System.out.println("The number of common subsequence is " + start.find(text1, text2));
-        }
-
-        public static int find(String text1, String text2)
-        {
-            if(text1.length() < text2.length())
+            for(int j = 0; j < text2.length(); j++)
             {
-                int[] num = new int[text2.length()];
-            }
-            else
-            {
-                int[] num = new int[text1.length()];
-            }
-
-            for(int i = 0; i < text1.length(); i++)
-            {
-                for(int j = 0; j < text2.length(); j++)
+                if (text1.charAt(i) == text2.charAt(j))
                 {
-                    if (text1.charAt(i) == text2.charAt(j))
-                    {
-                        num[i] = j;
-                        break;
-                    }
+                    num[i] = text1.charAt(i);
+                    break;
                 }
             }
-
-            return num;
-
         }
 
+        return num;
 
+    }
 }
+
+ */
